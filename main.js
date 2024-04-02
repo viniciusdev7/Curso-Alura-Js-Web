8319852ebffed7fe8cf01925968ff7d1c4afdce7
Vinicius Oliveira <vinicius.oli7dev@gmail.com>
2024-04-02 17:13:57 -0300

    Adicionando Condicoes no codigo

--- main.js
+++ main.js
@@ -14,5 +14,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
+    tecla.onkeydown = function(evento){
+
+        console.log(evento.code);
+
+        if(evento.code == `Space`){
+            tecla.classList.add(`ativa`);
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove(`ativa`)
+    }
 } 
  
