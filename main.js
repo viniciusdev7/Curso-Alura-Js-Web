ab595453d959d61c9b39a926231b38dfb4083b0f
Vinicius Oliveira <vinicius.oli7dev@gmail.com>
2024-04-02 12:29:53 -0300

    Inicializando os Estudos JS - Curso 1

---
+++ main.js
@@ -0,0 +1,18 @@
+function tocaSom (idAudio) {
+    document.querySelector(idAudio).play(); 
+}
+
+const listaDeTeclas = document.querySelectorAll(`.tecla`);
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+} 
+ 
